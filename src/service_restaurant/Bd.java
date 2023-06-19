7b3b1c1ca225b9b42bb6b10c29169dda58c9c46e
melissandre88 <74959766+melissandre88@users.noreply.github.com>
2023-06-19 15:31:55 +0200

    ajout de Bd.java

---
+++ src/service_restaurant/Bd.java
@@ -0,0 +1,44 @@
+package service_restaurant;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Bd {
+
+    public ResultSet getCoordonnees(String login, String pwd) throws SQLException {
+        String url = "jdbc:oracle:thin:@charlemagne.iutnc.univ-lorraine.fr:1521:infodb";
+        Connection connection = DriverManager.getConnection(url, login, pwd);
+        java.sql.Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT nom-resto, coordonees FROM RESTAURANT");
+        return resultSet;
+    }
+
+    public void reserver(String nom, String prenom, int nb_convives, String tel, String resto, String login, String pwd) throws SQLException {
+        String url = "jdbc:oracle:thin:@charlemagne.iutnc.univ-lorraine.fr:1521:infodb";
+        Connection connection = DriverManager.getConnection(url, login, pwd);
+        java.sql.Statement statement = connection.createStatement();
+        ResultSet resultSet = statement.executeQuery("SELECT nom FROM CLIENT WHERE nom = '" + nom + "' AND prenom = '" + prenom + "'");
+        if (!resultSet.next()) {
+            statement.executeUpdate("INSERT INTO CLIENT VALUES (client_seq.nextval, '" + nom + "', '" + prenom + "', '" + tel + "')");
+        }
+        resultSet = statement.executeQuery("SELECT id_client FROM CLIENT WHERE nom = '" + nom + "' AND prenom = '" + prenom + "'");
+        resultSet.next();
+        int id_client = resultSet.getInt("id_client");
+        resultSet = statement.executeQuery("SELECT id_resto FROM RESTAURANT WHERE nom_resto = '" + resto + "'");
+        resultSet.next();
+        int id_resto = resultSet.getInt("id_resto");
+        resultSet = statement.executeQuery("SELECT num_table FROM TABL WHERE id_resto = " + id_resto + " AND num_table NOT IN (SELECT num_table FROM RESERVATION WHERE id_resto = " + id_resto + ")");
+        if (!resultSet.next()) {
+            throw new SQLException("Il n'y a plus de table disponible dans ce restaurant");
+        }else {
+            resultSet.next();
+            int num_table = resultSet.getInt("num_table");
+            statement.executeUpdate("INSERT INTO RESERVATION VALUES (reservation_seq.nextval, " + id_client + ", " + id_resto + ", " + num_table + ", " + nb_convives + ")");
+        }
+
+
+
+    }
+}
